5ba1b61f774aa4a564c7daa628a2f0ad86146aae
Deepraj4595 <151773148+Deepraj4595@users.noreply.github.com>
2025-12-11 23:31:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function openModal() {
+  let modal = document.getElementById("modal");
+  modal.style.display = "flex";
+  modal.classList.add("show");
+}
+
+function closeModal() {
+  let modal = document.getElementById(modal);
+  modal.classList.remove("show");
+  setTimeout(function () {
+    modal.style.display = "none";
+  }, 300);
+}
